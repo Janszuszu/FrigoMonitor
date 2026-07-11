406fa14fb4f7e9929735db05c728295a1fb5b50d
Janszuszu <janszul92@gmail.com>
2026-07-11 02:01:04 +0200

    Fix alembic env.py to include backend directory in sys.path

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,7 @@ import sys
 
 # allow importing app package
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
 
 from app.database import Base
 from app.config import settings
