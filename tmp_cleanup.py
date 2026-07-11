6208c177fb874399d37c96d42dbeada9575f7e56
Janszuszu <janszul92@gmail.com>
2026-07-11 02:18:52 +0200

    Remove temp files

--- tmp_cleanup.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('/home/yan/FrigoMonitor/backend/frigomonitor.db')
-conn.execute('DROP TABLE IF EXISTS _alembic_tmp_sensors')
-conn.commit()
-conn.close()
-print('Cleaned up')
